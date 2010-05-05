be846db304f9d9a270b5ec5e767ef7fbf5030e00
tejonBiker <tejonBiker@a390e16f-b0b9-001a-816e-63014fadcbaa>
2010-05-05 20:00:02 +0000

    Clase de ejemplo del AFCU

---
+++ trunk/Classsport-mobile/src/hello/Classsport.java
@@ -0,0 +1,123 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hello;
+
+import javax.microedition.lcdui.*;
+import javax.microedition.midlet.*;
+
+import dicis.afcu.AFCU;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Vector;
+import javax.microedition.io.Connector;
+import javax.microedition.io.file.FileConnection;
+
+/**
+ * @author Tejon
+ */
+public class Classsport extends MIDlet implements CommandListener{
+
+    Form form = new Form("tejon");
+    Display display;
+
+
+    public void startApp() {
+
+        display = Display.getDisplay(this);
+        form.addCommand(new Command("salir",1,Command.EXIT));
+        form.setCommandListener(this);
+        Vector files=null;
+
+        //for(int i=0;i<200;i++){files = AFCU.getFilesWith("blend");}
+
+
+        /*
+        try{
+
+        form.append(AFCU.setUpWorkSpace());
+
+        }catch(SecurityException e)
+        {form.append("cannot setup the workspace in automatic mode, ");}
+         */
+
+        String ws = AFCU.setUpWorkSpace();
+
+        if(ws==null)
+            form.append("No se pudo crear el WorkSpace");
+        else
+            form.append("El WorkSpace es:\n "+ws);
+
+        try{
+        form.append(Image.createImage("/logo.png"));
+
+        FileConnection fc = (FileConnection)Connector.open(ws + "readme.txt");
+        //DataInputStream fileStream = fc.openDataInputStream();
+        DataInputStream fileStream = fc.openDataInputStream();
+        byte data[]=new byte[(int)fc.fileSize()];
+        int readData;
+
+        form.append(fc.getURL()+", r:"+fc.canRead()+", s: "+fc.fileSize()+",w:"+fc.canWrite());
+
+        StringBuffer stringData = new StringBuffer();
+
+        System.out.println(fileStream.available());
+
+        /*
+        readData = fileStream.read(data);
+
+
+        for(int i=0;i<readData;i++)
+        {
+            stringData.append((char)data[i]);
+        }*/
+
+        while(fileStream.available()!=0)
+            stringData.append(fileStream.readChar());
+
+        form.append(stringData.toString()+"\ntamaño del archivo (bytes): "+fc.fileSize());
+
+         fileStream.close();
+        fc.close();
+
+
+        }catch(IOException e)
+        {
+            e.printStackTrace();
+            form.append("no se pudo abrir el archivo readme");
+        }
+
+        /*
+        files = AFCU.getFilesWith("mp3");
+
+
+        if(files!=null)
+        {
+            for(int i=0;i<files.size();i++)
+            {
+                form.append((String)files.elementAt(i)+"\n");
+            }
+        }
+        else
+        {
+            form.append("No se detectaron volumenes");
+        }*/
+
+        display.setCurrent(form);
+    }
+
+    public void pauseApp() {
+    }
+
+    public void destroyApp(boolean unconditional) {
+    }
+
+    public void commandAction(Command cmnd, Displayable dsplbl) {
+
+        destroyApp(false);
+        notifyDestroyed();
+    }
+}
